041fc52302469c178eef61cef8765244f623c5f2
Nef007 <kaidalov.007@yandex.ru>
2024-03-16 12:46:06 +0300

    fix request order

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -163,8 +163,8 @@ class AccountController {
                     await request_bitmex(account.apikey, account.apisecret, 'GET', '/position',
                         {reverse: true}
                     ),
-                    await request_bitmex(account.apikey, account.apisecret, 'GET', '/order',
-                        {reverse: true, filter:{"open": true}}
+                    await request_bitmex(account.apikey, account.apisecret, 'GET', '/order?filter=%7B%22open%22%3A%20true%7D',
+                        {reverse: true, count: 100}
                     ),
 
 
